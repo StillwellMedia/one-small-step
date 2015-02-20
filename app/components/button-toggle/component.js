0460d89306093d8a7407741dbe6294f20a86a8e1
Lindsay Stillwell <stillwellmedia@gmail.com>
2015-02-20 00:17:18 -0500

    adding comments to toggleButton component

--- app/components/button-toggle/component.js
+++ app/components/button-toggle/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName: 'a',
 	
-	attributeBindings: ['href'],
+	attributeBindings: ['href'], // An <a> without an href is invalid
   	
   href: "#",
 
@@ -18,8 +18,11 @@ export default Ember.Component.extend({
 	to: 'fa-angle-down', // toggled Class
 
   toggleClass: function() {
-		
+		// Toggle the button class name between the classes defined in from and to variables.
+
 		if ( this.get('isToggled') ) {
+       // If this is toggled to true, set the class to 'fa-angle-down', 
+      // or whichever class is declared in that variable.
   			return this.get('to');
   		}
   		return this.get('from');
@@ -27,6 +30,7 @@ export default Ember.Component.extend({
   	}.property('isToggled'),
 
   	isVisible: function(){
+      // Sometimes it's useful to let an element continue to take up space in the dom
   		if ( this.get('visible') ) {
   			return '';
   		}
@@ -35,8 +39,8 @@ export default Ember.Component.extend({
   	}.property('visible'),
 
   	click: function() {
-  		this.toggleProperty('isToggled');
-  		this.sendAction();
-  		return false;
+  		this.toggleProperty('isToggled'); // Toggles this variable between true and false
+  		this.sendAction(); // Send a generic action that the parent can act upon
+  		return false; // return false so that the page does not reload
   	}
 });
